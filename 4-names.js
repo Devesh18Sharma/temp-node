6e1dddf51188439fa39d06a8e6ae697a8c22d28a
Devesh Sharma <97972618+Devesh18Sharma@users.noreply.github.com>
2023-02-27 23:55:30 +0530

    first commit

---
+++ 4-names.js
@@ -0,0 +1,6 @@
+//Local
+const secret='SUPER SECRET'
+//Shared
+const john='john'
+const peter='peter'
+module.exports={john,peter}
---
+++ 7.5-void.js
@@ -0,0 +1,10 @@
+//CommonJS, every file is module (By Default)
+//Modules-encapsulated code (only share minimum)
+const names=require('./4-names')
+const sayHi=require('./5-utils')
+const data=require('./6-Alternative-flavour')
+console.log(data);
+require('./7-mind-grenade')
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
